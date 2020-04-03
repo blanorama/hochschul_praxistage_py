f6798fd7de440c94fa4f80a3225bbce8353c0806
Eugen Blattner <eugen.blattner@mayflower.de>
2020-04-03 15:50:19 +0200

    add additionally needed condition

--- converter.py
+++ converter.py
@@ -15,9 +15,9 @@ def convert(arg: int) -> str:
     while arg > 0:
         for i in range(0, len(cheat)):
             if arg >= cheat[i][1]:
-                if i is not 0 and arg is cheat[i-1][1] - cheat[i][1]:
+                if i is not 0 and arg is not cheat[i][1] and arg < cheat[i-1][1] and arg >= cheat[i-1][1] - cheat[i][1]:
                     res += cheat[i][0] + cheat[i-1][0]
-                    arg -= cheat[i-1][1] + cheat[i][1]
+                    arg -= cheat[i-1][1] - cheat[i][1]
                     break
                 else:
                     res += cheat[i][0]
